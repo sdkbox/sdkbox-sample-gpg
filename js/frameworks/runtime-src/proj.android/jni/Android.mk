b490acc5066bde0dc5def4d18a72ef2b238c6279
Kai Zhao <kai@us.chukong-inc.com>
2016-08-16 20:26:34 -0700

    removed local paths; added setup with dependencies; added jars

--- js/frameworks/runtime-src/proj.android/jni/Android.mk
+++ js/frameworks/runtime-src/proj.android/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libgpg
-LOCAL_SRC_FILES := /Users/ibon/punchbox/test/gpg-sample2/js/frameworks/runtime-src/proj.android/gpg/lib/gnustl/$(TARGET_ARCH_ABI)/libgpg.a
+LOCAL_SRC_FILES := ../gpg/lib/gnustl/$(TARGET_ARCH_ABI)/libgpg.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 LOCAL_MODULE := cocos2djs_shared
@@ -24,7 +24,7 @@ LOCAL_LDLIBS := -landroid \
 -llog
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
-/Users/ibon/punchbox/test/gpg-sample2/js/frameworks/runtime-src/proj.android/gpg/include/ \
+../gpg/include/ \
 $(LOCAL_PATH)/..
 LOCAL_WHOLE_STATIC_LIBRARIES := PluginSdkboxPlay \
 sdkbox \
---
+++ js/frameworks/runtime-src/proj.android/libs/gps/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/kai/source/sdkbox-sample-googleplay/js/frameworks/runtime-src/proj.android/libs/gps/bin/AndroidManifest.xml : \
+/Users/kai/source/sdkbox-sample-googleplay/js/frameworks/runtime-src/proj.android/libs/gps/AndroidManifest.xml \
