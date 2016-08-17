aec94c1d44c89c1ac11725341f97f933784b7e85
hyperandroid <hyperandroid@gmail.com>
2016-08-17 17:29:41 +0200

    cpp sample. remove intermediate files for java gpg dependency.

--- cpp/proj.android/libs/gps/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/ibon/punchbox/test/sdkbox-sample-sdkboxgoogleplay2/cpp/proj.android/libs/gps/bin/AndroidManifest.xml : \
-/Users/ibon/punchbox/test/sdkbox-sample-sdkboxgoogleplay2/cpp/proj.android/libs/gps/AndroidManifest.xml \
