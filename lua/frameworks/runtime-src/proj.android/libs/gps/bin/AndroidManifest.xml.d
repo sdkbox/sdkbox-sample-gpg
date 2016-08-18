9f06fd58171c27a8cf7ad35e741cd0a06712fa38
mannewalis <=>
2016-08-18 12:30:35 -0700

    moving jars->libs

---
+++ lua/frameworks/runtime-src/proj.android/libs/gps/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/justin/cocos/cocos-service-center/plugins/sdkboxgoogleplay/temp/sdkbox-sample-sdkboxgoogleplay/lua/frameworks/runtime-src/proj.android/libs/gps/bin/AndroidManifest.xml : \
+/Users/justin/cocos/cocos-service-center/plugins/sdkboxgoogleplay/temp/sdkbox-sample-sdkboxgoogleplay/lua/frameworks/runtime-src/proj.android/libs/gps/AndroidManifest.xml \
