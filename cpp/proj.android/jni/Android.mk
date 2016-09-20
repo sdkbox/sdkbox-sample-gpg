4ea1c703c1d8371067f937c71f457d15faad1321
Nite Luo <darkdukey@gmail.com>
2016-09-20 12:32:43 -0700

    update android.mk for c++

--- cpp/proj.android/jni/Android.mk
+++ cpp/proj.android/jni/Android.mk
@@ -1,9 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libgpg
-LOCAL_SRC_FILES := ../gpg/lib/gnustl/$(TARGET_ARCH_ABI)/libgpg.a
-include $(PREBUILT_STATIC_LIBRARY)
 
 LOCAL_MODULE := cocos2dcpp_shared
 
@@ -37,7 +34,7 @@ LOCAL_WHOLE_STATIC_LIBRARIES := sdkbox \
 PluginSdkboxGooglePlay \
 gpg-1
 
-LOCAL_STATIC_LIBRARIES := cocos2dx_static PluginSdkboxGooglePlay sdkbox
+LOCAL_STATIC_LIBRARIES := cocos2dx_static
 
 include $(BUILD_SHARED_LIBRARY)
 $(call import-add-path, $(LOCAL_PATH))
