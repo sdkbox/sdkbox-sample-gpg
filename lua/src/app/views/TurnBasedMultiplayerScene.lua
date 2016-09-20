77b4d919c7ec85ad0ac6a1572635161694e222c4
mannewalis <=>
2016-09-20 10:26:38 -0700

    working show matches and leave logic added push/pop menu

--- lua/src/app/views/TurnBasedMultiplayerScene.lua
+++ lua/src/app/views/TurnBasedMultiplayerScene.lua
@@ -30,10 +30,15 @@ function TurnBasedMultiplayerScene:setupTestMenu()
 
     cc.MenuItemFont:setFontName("sans")
 
-    self._create_menu = cc.Menu:create(
+    self._match = nil
+    self._menus = {}
+
+    local menu = cc.Menu:create(
         cc.MenuItemFont:create("Show Match Inbox UI"):onClicked(function()
             gpg.Turnbased:ShowMatchInboxUI(function(o)
-                log:d(log:to_str(o))
+                if gpg:IsSuccess(o.result) then
+                    self:handleShowMatchInbox(o.match)
+                end
             end)
         end),
         cc.MenuItemFont:create("Quick Match"):onClicked(function()
@@ -65,41 +70,35 @@ function TurnBasedMultiplayerScene:setupTestMenu()
         end)
     )
 
-    self._match_menu = cc.Menu:create(
-        cc.MenuItemFont:create("Win"):onClicked(function()
-            gpg.Turnbased:ShowMatchInboxUI(function(o)
-                log:d(log:to_str(o))
-            end)
-        end),
-        cc.MenuItemFont:create("Lose"):onClicked(function()
-            gpg.Turnbased:ShowMatchInboxUI(function(o)
-                log:d(log:to_str(o))
-            end)
-        end),
-        cc.MenuItemFont:create("Leave"):onClicked(function()
-            self:leaveMatch(self._match)
-        end)
-    )
-
-    self:addMenu(self._create_menu, true)
-    self:addMenu(self._match_menu, false)
-
-    self:setupGame()
+    self:pushMenu(menu)
 end
 
-function TurnBasedMultiplayerScene:addMenu(menu, visible)
+function TurnBasedMultiplayerScene:pushMenu(menu)
     local size = cc.Director:getInstance():getWinSize()
     menu:alignItemsVerticallyWithPadding(5)
     menu:setPosition(size.width/2, size.height/2)
+
+    local count = #self._menus
+    if count > 0 then
+        local last = self._menus[1]
+        self:removeChild(last)
+    end
+
+    table.insert(self._menus, 1, menu)
+    menu:retain()
     self:addChild(menu)
-    menu:setVisible(visible)
 end
 
-function TurnBasedMultiplayerScene:setupGame()
-    self._match = nil
-    self._create_menu:setVisible(true)
-    self._match_menu:setVisible(false)
-
+function TurnBasedMultiplayerScene:popMenu()
+    local count = #self._menus
+    if count > 1 then
+        local last = self._menus[1]
+        self:removeChild(last)
+        table.remove(self._menus, 1)
+        last:release()
+        last = self._menus[1]
+        self:addChild(last)
+    end
 end
 
 function TurnBasedMultiplayerScene:handleMatchEvent(o)
@@ -107,39 +106,106 @@ function TurnBasedMultiplayerScene:handleMatchEvent(o)
     log:d(log:to_str(o))
 end
 
-function TurnBasedMultiplayerScene:playMatch(match)
-    self._match = match
-    self._create_menu:setVisible(false)
-    self._match_menu:setVisible(true)
+function TurnBasedMultiplayerScene:handleShowMatchInbox(match)
+    log:d("TurnBasedMultiplayerScene:handleShowMatchInbox")
     log:d(log:to_str(match))
+    if match.matchStatus == gpg.MatchStatus.MY_TURN then
+        self:playMatch(match)
+    elseif match.matchStatus == gpg.MatchStatus.THEIR_TURN then
+        self:manageMatch(match, true, true, false)
+    elseif match.matchStatus == gpg.MatchStatus.COMPLETED then
+        self:manageMatch(match, false, false, true)
+    elseif match.matchStatus == gpg.MatchStatus.EXPIRED then
+        self:manageMatch(match, false, false, false)
+    else
+        log:d("Unhandled status "..match.matchStatus)
+    end
 end
 
-function TurnBasedMultiplayerScene:leaveMatch(match)
-    if match.status == gpg.MatchStatus.MY_TURN then
+function TurnBasedMultiplayerScene:playMatch(match)
+    log:d("TurnBasedMultiplayerScene:playMatch")
+    self._match = match
+    log:d(log:to_str(match))
+
+    local menu = cc.Menu:create(
+        cc.MenuItemFont:create("Win"):onClicked(function()
+            self:popMenu()
+        end),
+        cc.MenuItemFont:create("Lose"):onClicked(function()
+            self:popMenu()
+        end),
+        cc.MenuItemFont:create("Leave"):onClicked(function()
+            self:leaveMatch()
+            self:popMenu()
+        end)
+    )
+
+    self:pushMenu(menu)
+end
+
+function TurnBasedMultiplayerScene:manageMatch(match, leave, cancel, rematch)
+    log:d("TurnBasedMultiplayerScene:manageMatch")
+    self._match = match
+    log:d(log:to_str(match))
+
+    if not (leave and cancel and rematch) then
+        log:d("Nothing to manage")
+        return
+    end
+
+    local menu = cc.Menu:create()
+
+    if (leave) then
+        menu:addChild(cc.MenuItemFont:create("Leave"):onClicked(function()
+            self:leaveMatch()
+            self:popMenu()
+        end))
+    end
+
+    if (cancel) then
+        menu:addChild(cc.MenuItemFont:create("Cancel"):onClicked(function()
+            self:cancelMatch()
+            self:popMenu()
+        end))
+    end
+
+    if (rematch) then
+        menu:addChild(cc.MenuItemFont:create("Rematch"):onClicked(function()
+            self:rematch()
+            self:popMenu()
+        end))
+    end
+
+    self:pushMenu(menu)
+end
+
+function TurnBasedMultiplayerScene:leaveMatch()
+    match = self._match
+    if match.matchStatus == gpg.MatchStatus.MY_TURN then
         gpg.Turnbased:LeaveMatchDuringMyTurn(match.id, match.suggestedNextParticipant.id, function(o)
             log:d(log:to_str(o))
-            self:setupGame()
         end)
     else
         gpg.Turnbased:LeaveMatchDuringTheirTurn(match.id, function(o)
             log:d(log:to_str(o))
-            self:setupGame()
         end)
     end
+end
+
+function TurnBasedMultiplayerScene:cancelMatch()
+    gpg.Turnbased:CancelMatch(self._match, function(o)
+        log:d(log:to_str(o))
+    end)
+end
+
+function TurnBasedMultiplayerScene:rematch()
+    gpg.Turnbased:Rematch(self._match, function(o)
+        if gpg:IsSuccess(o.result) then
+            self:playMatch(o.match)
+        end
+    end)
 end
 
 return TurnBasedMultiplayerScene
 
 
---[[
-
- flow for sample
-
- setup
- choose players -> create game
- 2 accept invitation
-
- 1 take turn
- 2 take turn -> finish game -> setup
-
- ]]
