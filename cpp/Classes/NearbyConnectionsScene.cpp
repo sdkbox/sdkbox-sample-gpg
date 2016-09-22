d25752b5e0c20daea264c4aa729fa21265b6543f
hugo <hugohuang1111@gmail.com>
2016-09-22 13:22:22 +0800

    add nearby cpp sample

---
+++ cpp/Classes/NearbyConnectionsScene.cpp
@@ -0,0 +1,9 @@
+//
+//  NearbyConnectionsScene.cpp
+//  sdkboxplay_cpp
+//
+//  Created by hugo on 16/9/22.
+//
+//
+
+#include "NearbyConnectionsScene.hpp"
---
+++ cpp/Classes/NearbyConnectionsScene.hpp
@@ -0,0 +1,14 @@
+//
+//  NearbyConnectionsScene.hpp
+//  sdkboxplay_cpp
+//
+//  Created by hugo on 16/9/22.
+//
+//
+
+#ifndef NearbyConnectionsScene_hpp
+#define NearbyConnectionsScene_hpp
+
+#include <stdio.h>
+
+#endif /* NearbyConnectionsScene_hpp */
